b4828351538aea0dc40e10796113308b7bb214bb
aranciro <aranciro@protonmail.com>
2021-05-30 18:42:07 +0200

    Added eslint and initial sources.

---
+++ twitch-viewer-alerter.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name            Twitch Viewer Alerter
+// @namespace       https://github.com/aranciro/
+// @version         0.1.0
+// @license         GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
+// @description     Configurable browser userscript that alerts when selected users join the chat.
+// @author          aranciro
+// @homepage        https://github.com/aranciro/Twitch-Viewer-Alerter
+// @supportURL      https://github.com/aranciro/Twitch-Viewer-Alerter/issues
+// @updateURL       https://raw.githubusercontent.com/aranciro/Twitch-Viewer-Alerter/master/twitch-viewer-alerter.user.js
+// @downloadURL     https://raw.githubusercontent.com/aranciro/Twitch-Viewer-Alerter/master/twitch-viewer-alerter.user.js
+// @icon            https://github.com/aranciro/Twitch-Viewer-Alerter/raw/master/res/twitch-viewer-alerter-icon32.png
+// @icon64          https://github.com/aranciro/Twitch-Viewer-Alerter/raw/master/res/twitch-viewer-alerter-icon64.png
+// @require         https://openuserjs.org/src/libs/sizzle/GM_config.min.js
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_registerMenuCommand
+// @grant           GM_addStyle
+// @include         *://*.twitch.tv/*
+// @run-at          document-idle
+// ==/UserScript==
